0a5b5bf6dfc1748334c6f1ac2e290efe0a6bd7d1
jonsalkin <50082962+jonsalkin@users.noreply.github.com>
2023-05-16 07:41:40 -0400

    Initial commit

---
+++ CustomTableView/CustomTableViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  CustomTableViewCell.swift
+//  CustomTableView
+//
+//  Created by Jon Salkin on 5/16/23.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var iconImageView: UIImageView!
+    @IBOutlet weak var label: UILabel!
+
+}
--- CustomTableView/ViewController.swift
+++ CustomTableView/ViewController.swift
@@ -7,13 +7,51 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+   
+    
+    //MARK: - IBOutlets
+    @IBOutlet weak var table: UITableView!
+    
+    
+    struct FamilyPics {
+        let title: String
+        let imageName: String
+    }
+    
+    let data: [FamilyPics] = [
+    FamilyPics(title: "Dad", imageName: "tableJon"),
+    FamilyPics(title: "Mom", imageName: "tableJulie"),
+    FamilyPics(title: "Mischa", imageName: "tableMischa"),
+    FamilyPics(title: "Michaela", imageName: "tableMiki"),
+    FamilyPics(title: "Millie", imageName: "tableMillie"),
+    FamilyPics(title: "Luna", imageName: "tableLuna"),
+    FamilyPics(title: "Willow", imageName: "tableWillow"),
+    ]
+    
+    //MARK: - VDL
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        table.dataSource = self
+        table.delegate = self
     }
 
 
+    //MARK: - Methods
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return data.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let familyPic = data[indexPath.row]
+        let cell = table.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! CustomTableViewCell
+        cell.label.text = familyPic.title
+        cell.iconImageView.image = UIImage(named: familyPic.imageName)
+        return cell
+    }
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 140
+    }
 }
 
